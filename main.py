1ac8c625ccde811eeed597c7bc07d2695c2efa42
Kyohei Moriyama(a.k.a sasukeh) <sasukeh@users.noreply.github.com>
2019-08-09 23:54:04 +0900

    update

--- main.py
+++ main.py
@@ -9,23 +9,8 @@ import threading
 """
 this class uses python threading technic, if you don't know threading in python properly. please refer to artcile below before you started using this.
     https://realpython.com/intro-to-python-threading/
-Timeoutを簡単に実装するTips:
+TimeoutTips:
     https://qiita.com/toshitanian/items/133b42355b7867f5c458
-
-@architecture
-get_responseとTimeoutを並列に実行して
-早く帰ってきたほうを正とする。
-Response codeが200、それ意外はFailed = timeoutということ。
-5秒のタイムアウトが6回、すなわちインターネット接続性が30秒失われた場合には、routerを強制的に再起動する。
-
-@TODO
-@Feature
-あと、電源の状態を見つつ、通電してなかったら通電させる機能もほしい。
-
-@TODO
-@Feature
-Googleだけじゃなく、他のサイトでもチェックする。
-両方のサイトの疎通を取れなくなったらインターネット接続性が失われたと判断できる。
 
 """
 
@@ -42,13 +27,11 @@ class Xxxx:
         print(str(start_time))
 
         try:
-            x = threading,Thread()
             result = requests.get(target_url)
         except:
             pass
         end_time = time.time()
         print(end_time-start_time)
-    
         return result
 
     def write_to_csv():
