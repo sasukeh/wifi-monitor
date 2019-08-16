0f37c251006a150666002731d49b01069d603af1
Kyohei Moriyama(a.k.a sasukeh) <sasukeh@users.noreply.github.com>
2019-08-16 21:50:32 +0900

    te

--- README.md
+++ README.md
@@ -13,3 +13,12 @@ csv_exporter.pyでCSVにひたすら下記のフォーマットで書き出す
 monitor.pyでそのCSVを監視
 
 両方のインスタンスについては、main.pyから操作する
+
+
+## file output
+os.
+
+## daemonize
+daemon.
+
+https://qiita.com/croquisdukke/items/9c5d8933496ba6729c78
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import requests
 from datetime import datetime, date
 import time
 import threading
+import csv
 
 """
 this class uses python threading technic, if you don't know threading in python properly. please refer to artcile below before you started using this.
@@ -14,32 +15,33 @@ TimeoutTips:
 
 """
 
-class Xxxx:
+class wifictrl:
     def __init__(self):
-        self.target_url = "https://www.google.com"
+        self.target_url = "https://www.google.co.jp"
 
     def main(self):
-        result = self.get_respons(self.target_url)
+        result = self.get_response(self.target_url)
         print(result)
+        self.write_to_csv(result)
    
-    def get_respons(self,target_url):
+    def get_response(self,target_url):
+        response = []
         start_time = time.time()
-        print(str(start_time))
-
-        try:
-            result = requests.get(target_url)
-        except:
-            pass
+        response.append(int(start_time))
+        result = requests.get(target_url)
+        response.append(result.status_code)
         end_time = time.time()
-        print(end_time-start_time)
-        return result
-
-    def write_to_csv():
-        pass
+        response.append(end_time-start_time)
+        return response
+    
+    def write_to_csv(self,result):
+        with open("result.csv","a") as file:
+            wrcsv = csv.writer(file,lineterminator='\n')
+            wrcsv.writerow(result)
 
 if __name__ == "__main__":
-  print("standalone/?")
-  xxxa = Xxxx()
+  print("boot with standalone mode")
+  xxxa = wifictrl()
   xxxa.main()
 
 
